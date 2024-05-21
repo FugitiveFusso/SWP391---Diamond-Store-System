6a2106c1c445e709dae4b13b9ec1b62fd6693081
FugitiveFusso <dodinhan1973@gmail.com>
2024-05-21 10:18:29 +0700

    new db

--- DB/Ring.sql
+++ DB/Ring.sql
@@ -10,33 +10,34 @@ CREATE TABLE [Role](
 
 CREATE TABLE [Diamond](
 	diamondID int NOT NULL primary key,
-	diamondName varchar NOT NULL,
-	origin varchar NOT NULL,
+	diamondName varchar(255) NOT NULL,
+	diamondImage varchar(8000) NOT NULL,
+	origin varchar(255) NOT NULL,
 	caratWeight int NOT NULL,
-	cut varchar NOT NULL,
-	color varchar NOT NULL,
-	clarity varchar NOT NULL
+	cut varchar(255) NOT NULL,
+	color varchar(255) NOT NULL,
+	clarity varchar(255) NOT NULL
 );
 
 CREATE TABLE [Collection](
 	collectionID int NOT NULL primary key,
-	collectionName varchar NOT NULL
+	collectionName varchar(255) NOT NULL
 );
 
 CREATE TABLE [Category](
 	categoryID int NOT NULL primary key,
-	categoryName varchar NOT NULL
+	categoryName varchar(255) NOT NULL
 );
 
 CREATE TABLE [User](
 	userID int NOT NULL primary key,
-	userName varchar NOT NULL,
-	[password] varchar NOT NULL,
-	firstName varchar NOT NULL,
-	lastName varchar NOT NULL,
-	phoneNumber varchar NOT NULL,
-	email varchar NOT NULL,
-	[address] text NOT NULL,
+	userName varchar(255) NOT NULL,
+	[password] varchar(255) NOT NULL,
+	firstName varchar(255) NOT NULL,
+	lastName varchar(255) NOT NULL,
+	phoneNumber varchar(255) NOT NULL,
+	email varchar(255) NOT NULL,
+	[address] varchar(8000) NOT NULL,
 	point int NOT NULL,
 	roleID int NOT NULL foreign key REFERENCES [Role](roleID)
 );
@@ -57,6 +58,15 @@ CREATE TABLE [Certificate](
 	diamondID int NOT NULL foreign key REFERENCES [Diamond](diamondID)
 );
 
+CREATE TABLE [Voucher](
+	voucherID int NOT NULL primary key,
+	voucherImage varchar(8000) NOT NULL,
+	voucherName varchar(255) NOT NULL,
+	[description] varchar(max) NOT NULL,
+	coupon varchar(255) NOT NULL,
+	percentage int NOT NULL
+);
+
 CREATE TABLE [RingSize](
 	ringID int foreign key REFERENCES [Ring](ringID),
 	size8 int NOT NULL,
@@ -69,14 +79,14 @@ CREATE TABLE [Order](
 	userID int NOT NULL foreign key REFERENCES [User](userID),
 	orderDate date NOT NULL,
 	status bit,
-	delivered varchar
+	delivered varchar(255)
 );
 
 CREATE TABLE [OrderDetails](
 	orderID int NOT NULL foreign key REFERENCES [Order](orderID),
 	ringID int NOT NULL foreign key REFERENCES [Ring](ringID),
-	diamondID int NOT NULL,
-	voucherID int NOT NULL,
+	diamondID int NOT NULL foreign key REFERENCES [Diamond](diamondID),
+	voucherID int NOT NULL foreign key REFERENCES [Voucher](voucherID),
 	totalPrice float NOT NULL
 );
 
@@ -84,14 +94,6 @@ CREATE TABLE [ProcessOrder](
 	processID int NOT NULL primary key,
 	orderID int NOT NULL foreign key REFERENCES [Order](orderID),
 	userID int NOT NULL foreign key REFERENCES [User](userID)
-);
-
-CREATE TABLE [Voucher](
-	voucherID int NOT NULL primary key,
-	voucherName varchar(255) NOT NULL,
-	[description] varchar(max) NOT NULL,
-	coupon varchar(255) NOT NULL,
-	percentage int NOT NULL
 );
 
 CREATE TABLE [Warranty](
@@ -104,11 +106,13 @@ CREATE TABLE [Bill](
 	billID int NOT NULL primary key,
 	userID int NOT NULL foreign key REFERENCES [User](userID),
 	orderID int NOT NULL foreign key REFERENCES [Order](orderID),
-	certificateID int NOT NULL foreign key REFERENCES [Certificate](certificateID)
+	certificateID int NOT NULL foreign key REFERENCES [Certificate](certificateID),
+	warrantyID int NOT NULL foreign key REFERENCES [Warranty](warrantyID),
 );
 
 CREATE TABLE [Post](
-	postName varchar(255) NOT NULL,
+	postID int NOT NULL primary key,
+	postName varchar(8000) NOT NULL,
 	description varchar(max) NOT NULL,
 	postImage image NOT NULL
 );
