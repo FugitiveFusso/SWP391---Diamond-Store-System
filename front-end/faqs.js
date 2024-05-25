f56bbde15d3988048988a84b35b53ce9002123ea
LAK <lekhoa2703@gmail.com>
2024-05-26 00:17:28 +0700

    Upload FAQs and update ringmeasuring,css

---
+++ front-end/faqs.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var dropdownBtns = document.querySelectorAll('.dropdown-btn');
+  
+    dropdownBtns.forEach(function(btn) {
+      var dropdownContent = btn.nextElementSibling;
+      var arrowIcon = btn.querySelector('i');
+  
+      btn.addEventListener('click', function () {
+        dropdownContent.classList.toggle('show');
+        arrowIcon.classList.toggle('fa-angle-down');
+        arrowIcon.classList.toggle('fa-angle-up');
+      });
+    });
+  });
+  
