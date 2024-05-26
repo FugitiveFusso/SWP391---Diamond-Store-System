64c53d74d35273192b1f6ce69c4ceccfad26d9a2
Le Anh Khoa <lekhoa2703@gmail.com>
2024-05-26 21:15:27 +0700

    Update login.jsp and upload css,js file to login.jsp

---
+++ back-end/FUJelwery/web/js/login.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const roleSelect = document.getElementById('roleSelect');
+
+    roleSelect.addEventListener('change', function() {
+        const selectedValue = this.value;
+        
+        if (selectedValue !== "0") { // Ignore the "Select your role" option
+            let url = '';
+
+            switch (selectedValue) {
+                case "1":
+                    url = 'userlogin.jsp';
+                    break;
+                case "2":
+                    url = 'admin.jsp';
+                case "3":
+                    url = 'managerlogin.jsp';
+                    break;
+                case "4":
+                    url = 'saleslogin.jsp';
+                    break;
+                case "5":
+                    url = 'deliverystafflogin.jsp';
+                    break;
+                default:
+                    url = 'admin.jsp'; // Default URL or handle error
+            }
+
+            if (url) {
+                window.location.href = url;
+            }
+        }
+    });
+});
