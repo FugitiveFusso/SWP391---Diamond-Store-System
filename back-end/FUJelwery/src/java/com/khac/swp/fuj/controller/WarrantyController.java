67016fd76a546e7bec2c3da023d7a9b36b644c4b
CuongNP284 <phucuong479@gmail.com>
2024-06-02 15:33:34 +0700

    upload Warranty DAO and Controller, xml

---
+++ back-end/FUJelwery/src/java/com/khac/swp/fuj/controller/WarrantyController.java
@@ -0,0 +1,227 @@
+package com.khac.swp.fuj.controller;
+
+import com.khac.swp.fuj.utils.DBUtils;
+import com.khac.swp.fuj.warranty.WarrantyDAO;
+import com.khac.swp.fuj.warranty.WarrantyDTO;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class WarrantyController extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            WarrantyDAO warrantyDAO = new WarrantyDAO();
+            String action = request.getParameter("action");
+            String keyword = request.getParameter("keyword");
+            if (keyword == null) {
+                keyword = "";
+            }
+            String sortCol = request.getParameter("colSort");
+
+            HttpSession session = request.getSession(false);
+            if (session == null || session.getAttribute("salessession") == null) {
+                response.sendRedirect("saleslogin.jsp");
+                return;
+            } else if (action == null || action.equals("list")) {//lists
+
+                WarrantyDAO dao = new WarrantyDAO();
+                List<WarrantyDTO> list = dao.getAllWarranty(keyword, sortCol);
+                request.setAttribute("warrantylist", list);
+
+                request.getRequestDispatcher("/warrantylist.jsp").forward(request, response);
+
+            } else if (action.equals("details")) {//details
+
+                Integer id = null;
+                try {
+                    id = Integer.parseInt(request.getParameter("id"));
+                } catch (NumberFormatException ex) {
+                    log("Parameter id has wrong format.");
+                }
+
+                WarrantyDTO warranty = null;
+                if (id != null) {
+                    warranty = warrantyDAO.load(id);
+                }
+
+                request.setAttribute("warranty", warranty);//object
+                RequestDispatcher rd = request.getRequestDispatcher("warrantydetails.jsp");
+                rd.forward(request, response);
+            } else if (action.equals("edit")) {//edit
+                Integer id = null;
+                try {
+                    id = Integer.parseInt(request.getParameter("id"));
+                } catch (NumberFormatException ex) {
+                    log("Parameter id has wrong format.");
+                }
+
+                WarrantyDTO warranty = null;
+                if (id != null) {
+                    warranty = warrantyDAO.load(id);
+                }
+
+                request.setAttribute("warranty", warranty);
+                request.setAttribute("nextaction", "update");
+                RequestDispatcher rd = request.getRequestDispatcher("warrantyedit.jsp");
+                rd.forward(request, response);
+
+            } else if (action.equals("create")) {//create
+                WarrantyDTO warranty = new WarrantyDTO();
+                request.setAttribute("warranty", warranty);
+                request.setAttribute("nextaction", "insert");
+                RequestDispatcher rd = request.getRequestDispatcher("warrantyedit.jsp");
+                rd.forward(request, response);
+
+            } else if (action.equals("update")) {//update
+                Integer warrantyid = null;
+                try {
+                    warrantyid = Integer.parseInt(request.getParameter("id"));
+                } catch (NumberFormatException ex) {
+                    log("Parameter id has wrong format.");
+                }
+                String warrantyname = request.getParameter("warrantyName");
+                String warrantyimage = request.getParameter("warrantyImage");
+                Integer month = null;
+                try {
+                    month = Integer.parseInt(request.getParameter("warrantyMonth"));
+                } catch (NumberFormatException ex) {
+                    log("Parameter month has wrong format.");
+                }
+                String description = request.getParameter("warrantyDescription");
+                String type = request.getParameter("warrantyType");
+                String startDate = request.getParameter("startDate");
+                String tac = request.getParameter("termsAndConditions");
+                WarrantyDTO warranty = null;
+                if (warrantyid != null) {
+                    warranty = warrantyDAO.load(warrantyid);
+                }
+                warranty.setId(warrantyid);
+                warranty.setName(warrantyname);
+                warranty.setImage(warrantyimage);
+                warranty.setMonth(month);
+                warranty.setDescription(description);
+                warranty.setType(type);
+                warranty.setStartdate(startDate);
+                warranty.setTermsandconditions(tac);
+                warrantyDAO.update(warranty);
+
+                request.setAttribute("warranty", warranty);
+                RequestDispatcher rd = request.getRequestDispatcher("warrantydetails.jsp");
+                rd.forward(request, response);
+
+            } else if (action.equals("insert")) {//insert
+                try {
+                    Connection conn = DBUtils.getConnection();
+                    int warrantyid = 0;
+                    String warrantyname = request.getParameter("warrantyName");
+                    String warrantyimage = request.getParameter("warrantyImage");
+                    Integer month = null;
+                    try {
+                        month = Integer.parseInt(request.getParameter("warrantyMonth"));
+                    } catch (NumberFormatException ex) {
+                        log("Parameter month has wrong format.");
+                    }
+                    String description = request.getParameter("warrantyDescription");
+                    String type = request.getParameter("warrantyType");
+                    String startDate = request.getParameter("startDate");
+                    String tac = request.getParameter("termsAndConditions");
+
+                    PreparedStatement ps = conn.prepareStatement("select max(warrantyID) from [Warranty]");
+                    ResultSet rs = ps.executeQuery();
+                    if (rs.next()) {
+                        warrantyid = rs.getInt(1);
+                        warrantyid++;
+                    }
+                    WarrantyDTO warranty = new WarrantyDTO();
+                    warranty.setId(warrantyid);
+                    warranty.setName(warrantyname);
+                    warranty.setImage(warrantyimage);
+                    warranty.setMonth(month);
+                    warranty.setDescription(description);
+                    warranty.setType(type);
+                    warranty.setStartdate(startDate);
+                    warranty.setTermsandconditions(tac);
+                    
+                    warrantyDAO.insert(warranty);
+                    request.setAttribute("warranty", warranty);
+                    RequestDispatcher rd = request.getRequestDispatcher("warrantydetails.jsp");
+                    rd.forward(request, response);
+                } catch (SQLException ex) {
+                    System.out.println("Insert warranty error!" + ex.getMessage());
+                    ex.printStackTrace();
+                }
+
+            } else if (action.equals("delete")) {//delete
+
+                Integer id = null;
+                try {
+                    id = Integer.parseInt(request.getParameter("id"));
+                } catch (NumberFormatException ex) {
+                    log("Parameter id has wrong format.");
+                }
+
+                warrantyDAO.delete(id);
+
+                List<WarrantyDTO> list = warrantyDAO.getAllWarranty(keyword, sortCol);
+                request.setAttribute("warrantylist", list);
+                RequestDispatcher rd = request.getRequestDispatcher("warrantylist.jsp");
+                rd.forward(request, response);
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- back-end/FUJelwery/src/java/com/khac/swp/fuj/warranty/WarrantyDAO.java
+++ back-end/FUJelwery/src/java/com/khac/swp/fuj/warranty/WarrantyDAO.java
@@ -1,5 +1,187 @@
 package com.khac.swp.fuj.warranty;
 
+import com.khac.swp.fuj.utils.DBUtils;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class WarrantyDAO {
 
+    public List<WarrantyDTO> getAllWarranty(String keyword, String sortCol) {
+        List<WarrantyDTO> list = new ArrayList<WarrantyDTO>();
+        try {
+            Connection con = DBUtils.getConnection();
+            String sql = " select warrantyID, warrantyName, warrantyImage, warrantyMonth, warrantyDescription, warrantyType, startDate, endDate, termsAndConditions from Warranty ";
+            if (keyword != null && !keyword.isEmpty()) {
+                sql += " where warrantyName like ? or warrantyMonth like ? or startDate like ? or endDate like ? or termsAndConditions like ? ";
+            }
+
+            if (sortCol != null && !sortCol.isEmpty()) {
+                sql += " ORDER BY " + sortCol + " ASC ";
+            }
+
+            PreparedStatement stmt = con.prepareStatement(sql);
+
+            if (keyword != null && !keyword.isEmpty()) {
+                stmt.setString(1, "%" + keyword + "%");
+                stmt.setString(2, "%" + keyword + "%");
+                stmt.setString(3, "%" + keyword + "%");
+                stmt.setString(4, "%" + keyword + "%");
+                stmt.setString(5, "%" + keyword + "%");
+
+            }
+            ResultSet rs = stmt.executeQuery();
+            if (rs != null) {
+                while (rs.next()) {
+
+                    int id = rs.getInt("warrantyID");
+                    String name = rs.getString("warrantyName");
+                    String image = rs.getString("warrantyImage");
+                    int month = rs.getInt("warrantyMonth");
+                    String description = rs.getString("warrantyDescription");
+                    String type = rs.getString("warrantyType");
+                    String startdate = rs.getString("startDate");
+                    String enddate = rs.getString("endDate");
+                    String tac = rs.getString("termsAndConditions");
+
+                    WarrantyDTO warranty = new WarrantyDTO();
+                    warranty.setId(id);
+                    warranty.setName(name);
+                    warranty.setImage(image);
+                    warranty.setMonth(month);
+                    warranty.setDescription(description);
+                    warranty.setType(type);
+                    warranty.setStartdate(startdate);
+                    warranty.setEnddate(enddate);
+                    warranty.setTermsandconditions(tac);
+                    list.add(warranty);
+                }
+            }
+
+            con.close();
+        } catch (SQLException ex) {
+            System.out.println("Error in servlet. Details:" + ex.getMessage());
+            ex.printStackTrace();
+        }
+        return list;
+    }
+
+    public WarrantyDTO load(int warrantyID) {
+
+        String sql = "select warrantyID, warrantyName, warrantyImage, warrantyMonth, warrantyDescription, warrantyType, startDate, endDate, termsAndConditions from Warranty where warrantyID = ?";
+
+        try {
+
+            Connection conn = DBUtils.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, warrantyID);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+
+                int id = rs.getInt("warrantyID");
+                String name = rs.getString("warrantyName");
+                String image = rs.getString("warrantyImage");
+                int month = rs.getInt("warrantyMonth");
+                String description = rs.getString("warrantyDescription");
+                String type = rs.getString("warrantyType");
+                String startdate = rs.getString("startDate");
+                String enddate = rs.getString("endDate");
+                String tac = rs.getString("termsAndConditions");
+
+                WarrantyDTO warranty = new WarrantyDTO();
+                warranty.setId(id);
+                warranty.setName(name);
+                warranty.setImage(image);
+                warranty.setMonth(month);
+                warranty.setDescription(description);
+                warranty.setType(type);
+                warranty.setStartdate(startdate);
+                warranty.setEnddate(enddate);
+                warranty.setTermsandconditions(tac);
+                return warranty;
+            }
+        } catch (SQLException ex) {
+            System.out.println("Query User error!" + ex.getMessage());
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public Integer insert(WarrantyDTO warranty) {
+        String sql = "INSERT INTO [Warranty] (warrantyID, warrantyName, warrantyImage, warrantyMonth, warrantyDescription, warrantyType, startDate, termsAndConditions) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        try {
+
+            Connection conn = DBUtils.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setInt(1, warranty.getId());
+            ps.setString(2, warranty.getName());
+            ps.setString(3, warranty.getImage());
+            ps.setInt(4, warranty.getMonth());
+            ps.setString(5, warranty.getDescription());
+            ps.setString(6, warranty.getType());
+            ps.setString(7, warranty.getStartdate());
+            ps.setString(8, warranty.getTermsandconditions());
+
+            ps.executeUpdate();
+            conn.close();
+            return warranty.getId();
+        } catch (SQLException ex) {
+            System.out.println("Insert Warranty error!" + ex.getMessage());
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public boolean update(WarrantyDTO warranty) {
+        String sql = "UPDATE [Warranty] SET warrantyName = ?, warrantyImage = ?, warrantyMonth = ?, warrantyDescription = ?, warrantyType = ?, startDate = ?, termsAndConditions = ? WHERE warrantyID = ? ";
+        try {
+
+            Connection conn = DBUtils.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setInt(8, warranty.getId());
+            ps.setString(1, warranty.getName());
+            ps.setString(2, warranty.getImage());
+            ps.setInt(3, warranty.getMonth());
+            ps.setString(4, warranty.getDescription());
+            ps.setString(5, warranty.getType());
+            ps.setString(6, warranty.getStartdate());
+            ps.setString(7, warranty.getTermsandconditions());
+
+            ps.executeUpdate();
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println("Update Warranty error!" + ex.getMessage());
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    /*
+    Delete student 
+     */
+    public boolean delete(int id) {
+        String sql = "DELETE [Warranty] WHERE warrantyID = ? ";
+        try {
+
+            Connection conn = DBUtils.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setInt(1, id);
+
+            ps.executeUpdate();
+            conn.close();
+            return true;
+        } catch (SQLException ex) {
+            System.out.println("Delete Warranty error!" + ex.getMessage());
+            ex.printStackTrace();
+        }
+
+        return false;
+    }
 }
